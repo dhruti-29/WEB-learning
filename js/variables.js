8e9accf8f28643fb2fe02dc2ffac52bf8fb209bb
dhruti viradiya <25cs113@charusat.edu.in>
2026-06-14 20:19:30 +0530

    add js code file

---
+++ js/variables.js
@@ -0,0 +1,61 @@
+
+
+//variables
+age = 24;
+name="dhruti";
+X =null;
+y = undefined;
+/* isfollw = true; */
+
+
+console.log(name);
+console.log(X);
+// = means assignment opreator 
+
+let age = 24;
+const a = 34;
+var e = 45;
+ 
+
+/* varible rules :
+
+apple != APPLE this two are different in js
+ only abc  , 123, _ are allowed and $ is allowed
+ space is not allowed
+ first letter must be a - z ya A - Z  ya _ ya $
+reserved words cannot be variable names. like consloe
+
+
+
+
+let : for varr.  declare ---> varriable can be re-declare and updated. A global scope varible 
+var : for varr.  declare  - old 
+const : cannot be re-declared 
+
+
+
+{code  }  
+
+
+
+const product = {
+title: "ball pen" ,
+rating: 4,
+offer: 5,
+price: 270,
+}
+
+
+instagram bio ::
+const peapr{
+name: "@shradhha kahpara",
+posts: 195,
+followers: 569,
+follwing: 4,
+isfollow: true,
+bio:apna college,
+}
+
+
+*/
+
