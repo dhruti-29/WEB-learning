edd2bd5c768c00abbf0bbf94173f22dc91ccafb7
dhruti viradiya <25cs113@charusat.edu.in>
2026-06-15 21:56:18 +0530

    add js code file

---
+++ js/string.js
@@ -0,0 +1,32 @@
+/* let str="dhruti";
+ */
+
+let str='dhruti';
+
+let line=`this is new type of string its called tempalte literal`;
+
+/* without this */
+
+let obj = {
+item: "pen" ,
+price: 10 ,
+
+};
+
+console.log("item",obj.item ,"price :", obj.price)
+
+
+/* with this */
+let output = `item ${obj.item} price : ${obj.price}`;
+console.log(output);
+
+
+
+
+/* other way */
+let output2 = `this is new type of string its called tempalte literal ${1+2+3}`;
+
+
+console.log("dhruti \n viradiya");
+console.log("dhruti \t viradiya");
+/* dhruti \t viradiya length , \t is one charcharter not two  */
