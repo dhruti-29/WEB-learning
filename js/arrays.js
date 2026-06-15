0a483a2117228405020ba070fb915684502f683a
dhruti viradiya <25cs113@charusat.edu.in>
2026-06-15 21:55:11 +0530

    add js code file

---
+++ js/arrays.js
@@ -0,0 +1,26 @@
+/* let h= ["dhruti" ,"drashti" ,"misti" , "ram"];
+
+for(let i=0;i<h.length ; i++){
+    console.log(h[i]);
+} */
+
+
+   /*  let marks = [33, 45, 56, 67, 78];
+let sum = 0;
+
+for (let val of marks) {
+    sum += val;
+}
+
+let avg = sum / marks.length;
+
+console.log(`avg marks of the class = ${avg}`); */
+
+
+
+/* let guj=["suart" , "jnd"];
+let mp=["mumabai" ,"lonavala"];
+
+let india =guj.concat(mp);
+console.log(india);
+ */
