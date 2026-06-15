b324462cba980d8415a4468370644767da2fd6a4
dhruti viradiya <25cs113@charusat.edu.in>
2026-06-15 21:56:03 +0530

    add js code file

---
+++ js/v_2_q_1.js
@@ -0,0 +1,14 @@
+/* To check if a number entered by a user is a multiple of 5 in JavaScript,
+ you can use the modulus operator (%), which returns the remainder of a division. */
+
+
+
+ let num =prompt("input any number");
+ console.log(num);
+
+ if(num%5==0){
+    console.log("num is multiple of 5");
+ }
+ else{
+    console.log("no!!");
+ }
