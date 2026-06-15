48cb256ed4f55f0370cb2ecfb523fe28d6d5683e
dhruti viradiya <25cs113@charusat.edu.in>
2026-06-15 21:56:51 +0530

    add js code file

---
+++ js/v_3_q_2.js
@@ -0,0 +1,13 @@
+/* number game
+ */
+
+let num = 30;
+let newnum = prompt("add number : ");
+
+
+
+while(newnum != num){
+    newnum = prompt("add again num :");
+}
+
+console.log("number is correct");
