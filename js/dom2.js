47968b587c95c4e804593c8ebbb5f5694cd8226e
dhruti viradiya <25cs113@charusat.edu.in>
2026-06-22 23:02:18 +0530

    add js code file

---
+++ js/dom2.js
@@ -0,0 +1,22 @@
+/* let div = document.querySelector("div");
+console.log(div);
+
+let id = div.getAttribute("id");
+console.log(id); */
+
+/* let para = document.querySelector("p");
+console.log(para.setAttribute("class" , "newclass")); */
+
+
+
+/* for acsses style in js  */
+/* let div = document.querySelector("div");
+div.style.backgroundColor = "green"; */
+
+let btn = document.createElement("button");
+btn.innerText = "click me ";
+console.log(btn);
+
+let div = document.querySelector("div");
+div.append(btn);
+
